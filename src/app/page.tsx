4aced3054b929bb95366cf81736bbdb86cdf950e
taufiqelahi <taufiqelahiiceiu@gmail.com>
2023-11-16 15:05:54 +0600

    intial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,24 @@
+
+
+const Home = () => {
+  return (
+    <div className="justify-center">
+      <div className="text-left px-10 py-10 text-2xl">
+      
+        NoteApp
+        
+        <div className=" flex py-10 gap-x-10 text-sm">
+        <button className=" bg-blue-800 text-white  h-10 w-20 rounded-md " >Sign In</button>
+        
+          <button className=" bg-blue-800 text-white  h-10 w-20 rounded-md ">Signup</button>
+        </div>
+      </div>
+      
+
+      
+
+    </div>
+  )
+}
+
+export default Home
