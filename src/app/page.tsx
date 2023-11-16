18e65834a91df58e3466cc9fd09bdfe279f14ce9
taufiqelahi <taufiqelahiiceiu@gmail.com>
2023-11-16 18:15:22 +0600

    create login and sign up pages

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,25 @@
+import Link from "next/link"
 
 
 const Home = () => {
   return (
     <div className="justify-center">
       <div className="text-left px-10 py-10 text-2xl">
-      
+
         NoteApp
-        
-        <div className=" flex py-10 gap-x-10 text-sm">
-        <button className=" bg-blue-800 text-white  h-10 w-20 rounded-md " >Sign In</button>
-        
-          <button className=" bg-blue-800 text-white  h-10 w-20 rounded-md ">Signup</button>
+
+        <div className=" flex py-10 gap-x-10 text-sm ">
+          <Link className="flex items-center justify-center bg-blue-800 text-white h-10 w-20 rounded-md " href="/login"
+
+          >Sign In</Link>
+
+          <Link className="flex items-center justify-center bg-blue-800 text-white  h-10 w-20 rounded-md  " href="/signup"
+          >Signup</Link>
         </div>
       </div>
-      
 
-      
+
+
 
     </div>
   )
