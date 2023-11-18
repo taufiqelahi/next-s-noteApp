f511b16d7bd7917c0772aca68d58944a2931baa2
taufiqelahi <taufiqelahiiceiu@gmail.com>
2023-11-18 17:54:48 +0600

    note edit and update to the supabse databse

--- src/app/notes/page.tsx
+++ src/app/notes/page.tsx
@@ -3,28 +3,29 @@
 
 
 import { Database } from '../types/supabase'
-
-
-
-import { useEffect, useState } from 'react'
+import { ChangeEvent, useEffect, useState } from 'react'
 import { supabaseBrowserClient } from '../../../utils/supabase_bowser'
 import { RealtimePostgresChangesPayload } from '@supabase/supabase-js'
-import {PencilSquareIcon} from '@heroicons/react/24/solid'
-
-
-
-
+import { PencilSquareIcon } from '@heroicons/react/24/solid'
+import Popup from '../components/popup'
 
 
 const NotePage = () => {
-  
+
 
   const [notes, setNotes] = useState<Array<Database>>([]);
-  const [titleText, settitleText] = useState('');
-  const [descriptionText, setdescriptionText] = useState('');
+  
+  const [note, setNote] = useState<Database>();
+
+
+
   const [isPopupOpen, setPopupOpen] = useState(false);
-  let notesData:any = []
-  const openPopup = () => {
+
+  let notesData: any = []
+
+  const openPopup = (n:any) => {
+   setNote(n)
+
     setPopupOpen(true);
   };
   const closePopup = () => {
@@ -52,13 +53,7 @@ const NotePage = () => {
 
 
   }
-  async function addNote() {
-    const { error } = await supabaseBrowserClient
-      .from('notes')
-      .insert({ title: titleText, description: descriptionText })
-      closePopup();
 
-  }
   function handleInserts(payload: RealtimePostgresChangesPayload<{ [key: string]: any }>): void {
     console.log(payload.eventType);
     if (payload.eventType == "INSERT") {
@@ -66,7 +61,7 @@ const NotePage = () => {
       setNotes(notesData)
     } else if (payload.eventType == "UPDATE") {
 
-      const index = notesData.findIndex((obj: { id: any })  => obj.id === payload.old.id);
+      const index = notesData.findIndex((obj: { id: any }) => obj.id === payload.old.id);
       if (index > -1) {
         console.log(index);
         notesData[index] = payload.new;
@@ -93,67 +88,31 @@ const NotePage = () => {
         <h2 className='my-4'>Notes:</h2>
         <ul >
           {notes.map((note, index) => (
-            <li className=' flex justify-between max-w-sm border border-blue-100 shadow-lg my-4  ' key={index}>
-              <div>
-              <h1 className=' text-lg font-bold'>{note.title}</h1>
-              <p className='  '> {note.description}</p>
-              </div>
-            
-              <PencilSquareIcon  className="h-6 w-6 text-blue-500" />
-              
-
+            <li  key={index}>
+           
+            <div className=' flex justify-between max-w-sm border border-blue-100 shadow-lg my-4  '>
+            <div>
+            <h1 className=' text-lg font-bold'>{note.title}</h1>
+            <p className='  '> {note.description}</p>
+          </div>
+          <PencilSquareIcon onClick={()=>openPopup(note)}  className="h-6 w-6 text-blue-500" />
+          </div>
+        
             </li>
           ))}
         </ul>
       </div>
       <div className='sm:col-span-2 md:col-span-1 '> Create new notes
         <div className='my-4 mx-4' >
-          
+
         </div>
         <div>
-          <button onClick={openPopup} className='h-10 w-20 rounded-xl bg-green-700 my-4'>
+          <button onClick={()=>openPopup({})} className='h-10 w-20 rounded-xl bg-green-700 my-4'>
             Create
           </button>
           {isPopupOpen && (
-        <div className="fixed top-0 left-0 w-full h-full bg-gray-800 bg-opacity-50 flex justify-center items-center">
-          <div className="bg-white p-6 rounded shadow-lg">
-            <h2 className="text-2xl font-bold mb-4">create notes</h2>
-
-            <div className="mb-4">
-              <label htmlFor="input1" className="block text-gray-700 text-sm font-bold mb-2">
-                Title:
-              </label>
-              <input
-                type="text"
-               
-                value={titleText}
-                onChange={(e) => settitleText(e.target.value)}
-                className="border rounded w-full p-2"
-              />
-            </div>
-
-            <div className="mb-4">
-              <label htmlFor="input2" className="block text-gray-700 text-sm font-bold mb-2">
-                Description:
-              </label>
-              <textarea
-               
-                value={descriptionText}
-                onChange={(e) => setdescriptionText(e.target.value)}
-                className="border rounded w-full p-2"
-              />
-            </div>
-
-            <button onClick={addNote} className="bg-blue-500 text-white p-2">
-              Submit
-            </button>
-
-            <button onClick={closePopup} className="bg-red-500 text-white p-2 mt-4">
-              Close
-            </button>
-          </div>
-        </div>
-      )}
+    <Popup note={note} onClose={closePopup} />
+          )}
         </div>
 
 
